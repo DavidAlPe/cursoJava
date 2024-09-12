85d99b8dbd28d848e1ce9a4af2e3375715a8497b
n134795 <n134795@GT06312N134795P.sgt.sgtech.corp>
2024-09-12 13:47:31 +0200

    Bucles while y doWhile

--- src/es/cursojava/inicio/bucle/EjemploDoWhile.java
+++ src/es/cursojava/inicio/bucle/EjemploDoWhile.java
@@ -3,8 +3,13 @@ package es.cursojava.inicio.bucle;
 public class EjemploDoWhile {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// Bucle DoWhile, hacer mientras se cumpla la condición, se va a ejecutar al menos una vez
+		int i = 0;
+		do {
+			System.out.println("Hola Amiguete --> "+ i + "  antes");
+			i++;
+			System.out.println("Hola Amiguete --> "+ i + "  después");
+		}while (i<8);
+		System.out.println("Fin bucle DoWhile");		
 	}
-
 }
--- src/es/cursojava/inicio/bucle/EjemploWhile.java
+++ src/es/cursojava/inicio/bucle/EjemploWhile.java
@@ -3,8 +3,14 @@ package es.cursojava.inicio.bucle;
 public class EjemploWhile {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// bucle WHILE se repote hasta que no se cumpla la condición de dicho bucle
+		
+		int i=0;
+		while (i<=8) {
+			System.out.println("Hola Amiguete --> " + i);
+			i++;
+		}
+		System.out.println("Fin bucle while");
 	}
 
 }
