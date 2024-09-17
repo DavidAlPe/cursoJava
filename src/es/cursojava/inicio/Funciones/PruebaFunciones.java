d3eef3f18f4e14023546d53130d9eefa286a53cb
n134795 <n134795@GT06312N134795P.sgt.sgtech.corp>
2024-09-17 11:26:29 +0200

    arreglo

--- src/es/cursojava/inicio/Funciones/PruebaFunciones.java
+++ src/es/cursojava/inicio/Funciones/PruebaFunciones.java
@@ -2,7 +2,7 @@ package es.cursojava.inicio.Funciones;
 
 public class PruebaFunciones {
 
-	public static void funcionPrueba() {
+	public static void main (String[] args) {
 		// llamada de funciones desde otra clase
 		System.out.println(Funciones.calculaAnioNacimientoP(25));
 	}
